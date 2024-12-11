0ced495514ba272b850125280118dd9a19fa5153
sgandeplli <sgandepalli@softility.com>
2024-12-11 18:56:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "region" {
 variable "cluster_name" {
   description = "The name of the Kubernetes cluster"
   type        = string
-  default     = "my-cluster" 
+  default     = "my-cluster11" 
 }
 
 variable "node_count" {
