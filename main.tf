5030b623abbba10c61d88e8e78b8cacbe3626725
sgandeplli <sgandepalli@softility.com>
2024-12-11 15:45:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,22 +2,14 @@ resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
 
-  initial_node_count = var.node_count
-
-  # Enable or disable deletion protection
   deletion_protection = false
+
+  initial_node_count = var.node_count
 
   node_config {
     machine_type = var.node_machine_type
     disk_size_gb = 30
-
-    # Optional: Add scopes for specific permissions
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
   }
 
-  # If using `remove_default_node_pool`, consider setting it to true 
-  # and managing node pools explicitly for better control
   remove_default_node_pool = false
 }
