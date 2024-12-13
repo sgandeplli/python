a9c3df83469f4dc6119c182e58c9580554b3c676
sgandeplli <sgandepalli@softility.com>
2024-12-13 13:27:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
----
-apiVersion: v1
-kind: Pod
-metadata:
-  name: nginx
-spec:
-  nodeName: controlplane
-  containers:
-  -  image: nginx
-     name: nginx
+# from werkzeug.urls import url_quote
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Code is Working'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
